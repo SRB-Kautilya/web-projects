5f840c1b759cbbfa66f5963b47f3b5b1b78efcf4
Shashank reddy Bobbala <shashankreddy444@gmail.com>
2024-02-10 21:16:58 -0600

    added vedio

---
+++ Browser-camera/index.js
@@ -0,0 +1,12 @@
+let video = document.querySelector("video");
+
+navigator.mediaDevices
+    .getUserMedia({
+        video: true,
+        audio: true
+    })
+    .then(function (mediaStream) {
+        video.srcObject = mediaStream;
+    }).catch(function (err) {
+        console.log(err);
+    });
